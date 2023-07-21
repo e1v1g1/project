52c00df88de07e3db2a9fb5f9a00fbe74d5de053
John <john@gmail.com>
2023-07-22 02:17:12 +0300

    Methods and properties lesson 17

---
+++ js/methods.js
@@ -0,0 +1,24 @@
+const str = "teSt";
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("g"));
+
+const logg = "Hello world";
+
+console.log(logg.slice(6, 11));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 3));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
