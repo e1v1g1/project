10374551cbf6e2ee9159b92c1cf65fa9a0b7352c
John <john@gmail.com>
2023-10-04 02:52:11 +0300

    Lesson 19 and 20

---
+++ 19_callback.js
@@ -0,0 +1,24 @@
+function first() {
+  //Do something
+  setTimeout(function () {
+    console.log(1);
+  }, 1500);
+}
+
+function second() {
+  console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+  console.log(`Я учу: ${lang}`);
+  callback();
+}
+
+function done() {
+  console.log("Я прошел этот урок");
+}
+
+learnJS("JavaScript", done);
---
+++ methods.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const str = "tesFRTt";
+const arr = [1, 2, 3];
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world";
+
+console.log(logg.slice(-3, -1));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+const testInt = parseInt(test);
+const testFloat = parseFloat(test);
+
+console.log(test);
+console.log(testInt);
+console.log(testFloat);
+
+console.log(parseInt(test));
+console.log(parseFloat(test));
+console.log(test);
